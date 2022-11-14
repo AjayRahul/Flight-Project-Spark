db5ef77240225c56a6e56c281dcb7edee8201e5a
Ajay Rahul <21rahulajay@gmail.com>
2022-11-14 21:22:43 +0530

    Resolved local build error

--- src/main/scala/org/example/main.scala
+++ src/main/scala/org/example/main.scala
@@ -2,9 +2,8 @@ package org.example
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.Dataset
-import org.example.main.flightOutputData
 
-object main{
+object main extends App{
 
   val spark = SparkSession.builder()
     .master("local[*]")
@@ -17,14 +16,20 @@ object main{
   val flightOutputData = org.example.flightOutputData
 
   val data: Dataset[flightInputData] = Seq(
-     flightInputData("Air India", 1569315038, 1569319183L)
+     flightInputData("Air India", 1569315038L, 1569319183L)
     ,flightInputData("Air France", 1569290498L, 1569298178L)
     ,flightInputData("Emirates", 1567318178L, 1567351838L)
     ,flightInputData("Etihad", 1569315018L, 1569319163L)
     ,flightInputData("Quatar Airways", 1569320498L, 1569319163L)
   ).toDS()
 
+  println("The input data before transformation: ")
+  data.show(false)
+
   val result: Dataset[flightOutputData] = data.transform(returnDF)
+
+  println("The Output data after transformation: ")
+  result.show(false)
 
   def returnDF(inp: Dataset[flightInputData]): Dataset[flightOutputData] = {
     inp.map(x => transformLogic(x))
--- src/main/scala/org/example/package.scala
+++ src/main/scala/org/example/package.scala
@@ -2,7 +2,7 @@ package org
 
 package object example {
 
-  case class flightInputData(flightName: String, arrivalTime: BigInt, departureTime: BigInt)
+  case class flightInputData(flightName: String, departureTime: BigInt, arrivalTime: BigInt)
   case class flightOutputData(flightName: String, duration: String)
 
 }
